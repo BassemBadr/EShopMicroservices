13b36dbc0b6218a1a504c596f92a384ff3a1f266
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-12-31 04:47:40 +0200

    implement endpoint and handler for delete product in catalog service

--- src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
+++ src/Services/Catalog/CatalogAPI/Products/DeleteProduct/DeleteProductEndpoint.cs
@@ -1,6 +1,30 @@
-﻿namespace CatalogAPI.Products.DeleteProduct
+﻿
+using CatalogAPI.Products.CreateProduct;
+
+namespace CatalogAPI.Products.DeleteProduct
 {
-    public class DeleteProductEndpoint
+    //public record DeleteProductRequest();
+
+    public record DeleteProductResponse(bool IsSucces);
+
+    public class DeleteProductEndpoint : ICarterModule
     {
+        public void AddRoutes(IEndpointRouteBuilder app)
+        {
+            app.MapDelete("/products/{id}", async (Guid id, ISender sender) =>
+            {
+                var result = await sender.Send(new DeleteProductCommand(id));
+
+                var response = result.Adapt<DeleteProductResponse>();
+
+                return Results.Ok(response);
+            })
+            .WithName("DeleteProduct")
+            .Produces<CreateProductResponse>(StatusCodes.Status200OK)
+            .ProducesProblem(StatusCodes.Status400BadRequest)
+            .ProducesProblem(StatusCodes.Status404NotFound)
+            .WithSummary("Delete Product")
+            .WithDescription("Delete Product");
+        }
     }
 }
