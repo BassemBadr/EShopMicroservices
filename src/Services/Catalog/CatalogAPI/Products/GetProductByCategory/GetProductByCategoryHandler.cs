9f1396da24bdebe2236f452616d64cc4377e8e62
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-12-31 03:42:48 +0200

    implement endpoint for get products by category handler and endpoint

--- src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/GetProductByCategory/GetProductByCategoryHandler.cs
@@ -1,6 +1,23 @@
-﻿namespace CatalogAPI.Products.GetProductByCategory
+﻿
+namespace CatalogAPI.Products.GetProductByCategory
 {
-    public class GetProductByCategoryHandler
+    public record GetProductByCategoryQuery(string Category) : IQuery<GetProductByCategoryResult>;
+
+    public record GetProductByCategoryResult(IEnumerable<Product> Products);
+
+    public class GetProductByCategoryQueryHandler
+        (IDocumentSession session, ILogger<GetProductByCategoryQueryHandler> logger)
+        : IQueryHandler<GetProductByCategoryQuery, GetProductByCategoryResult>
     {
+        public async Task<GetProductByCategoryResult> Handle(GetProductByCategoryQuery query, CancellationToken cancellationToken)
+        {
+            logger.LogInformation("GetProductByCategoryQueryHandler.Handle With @{query}", query);
+
+            var products = await session.Query<Product>()
+                .Where(p => p.Category.Contains(query.Category))
+                .ToListAsync(cancellationToken);
+
+            return new GetProductByCategoryResult(products);
+        }
     }
 }
