4fa09ee4dedd20d15a47cd7cc2cbdb7095980002
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2025-12-31 22:45:40 +0200

    implement pipeline fluent validator

--- src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/CreateProduct/CreateProductHandler.cs
@@ -1,15 +1,38 @@
-﻿namespace CatalogAPI.Products.CreateProduct
+﻿using Microsoft.Extensions.Logging;
+
+namespace CatalogAPI.Products.CreateProduct
 {
     public record CreateProductCommand(string Name, List<string> Category, string Description, string ImageFile, decimal Price)
         : ICommand<CreateProductResult>;
 
     public record CreateProductResult(Guid Id);
 
-    internal class CreateProductCommandHandler(IDocumentSession session)
+    public class CreateProductValidator:AbstractValidator<CreateProductCommand>
+    {
+        public CreateProductValidator()
+        {
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .MaximumLength(100).WithMessage("Product name must not exceed 100 characters.");
+            RuleFor(x => x.ImageFile)
+                .NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("At least one category is required.");
+            RuleFor(x => x.Description)
+                .MaximumLength(1000).WithMessage("Description must not exceed 1000 characters.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+        }
+    }
+
+    internal class CreateProductCommandHandler
+        (IDocumentSession session,ILogger<CreateProductCommandHandler> logger)
         : ICommandHandler<CreateProductCommand, CreateProductResult>
     {
         public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
         {
+            logger.LogInformation("CreateProductCommandHandler.Handle with {@command}", command);
+
             //  Business logic to create product
             Product product = new Product
             {
--- src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
+++ src/Services/Catalog/CatalogAPI/Products/UpdateProduct/UpdateProductHandler.cs
@@ -5,6 +5,26 @@ namespace CatalogAPI.Products.UpdateProduct
 
     public record UpdateProductResult(bool IsSuccess);
 
+    public class UpdateProductCommandValidator : AbstractValidator<UpdateProductCommand>
+    {
+        public UpdateProductCommandValidator()
+        {
+            RuleFor(x => x.Id)
+                .NotEmpty().WithMessage("Product Id is required.");
+            RuleFor(x => x.Name)
+                .NotEmpty().WithMessage("Product name is required.")
+                .Length(2,100).WithMessage("Product name must be between 2 and 100 characters.");
+            RuleFor(x => x.ImageFile)
+                .NotEmpty().WithMessage("ImageFile is required.");
+            RuleFor(x => x.Category)
+                .NotEmpty().WithMessage("At least one category is required.");
+            RuleFor(x => x.Description)
+                .MaximumLength(1000).WithMessage("Description must not exceed 1000 characters.");
+            RuleFor(x => x.Price)
+                .GreaterThan(0).WithMessage("Price must be greater than zero.");
+        }
+    }
+
     internal class UpdateProductCommandHandler
         (IDocumentSession session, ILogger<UpdateProductCommandHandler> logger)
         : ICommandHandler<UpdateProductCommand, UpdateProductResult>
