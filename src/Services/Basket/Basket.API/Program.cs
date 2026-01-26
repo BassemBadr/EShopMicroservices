d903bfb5a15d3aaf156be96ded078822e5124d35
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-01-27 01:29:54 +0200

    apply some settings for launching the services without opening the browser as i am using postman for the tests

--- src/Services/Basket/Basket.API/Program.cs
+++ src/Services/Basket/Basket.API/Program.cs
@@ -20,6 +20,21 @@ builder.Services.AddMarten(opts =>
 }).UseLightweightSessions();
 
 builder.Services.AddScoped<IBasketRepository, BasketRepository>();
+builder.Services.Decorate<IBasketRepository, CachedBasketRepository>();
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    options.Configuration = builder.Configuration.GetConnectionString("Redis");
+});
+
+//builder.Services.AddScoped<BasketRepository>();
+//builder.Services.AddScoped<IBasketRepository, CachedBasketRepository>(
+//    provider => {
+//        var basketRepo = provider.GetRequiredService<BasketRepository>();
+//        return new CachedBasketRepository(
+//            basketRepo,
+//            provider.GetRequiredService<IDistributedCache>());
+//    });
 
 builder.Services.AddExceptionHandler<CustomExceptionHandler>();
 
