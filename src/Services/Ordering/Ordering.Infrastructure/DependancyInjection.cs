b53e10b522b88bc7effa9931121a57da4fff726e
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-03-04 03:53:30 +0200

    add database configuration to ordering service

--- src/Services/Ordering/Ordering.Infrastructure/DependancyInjection.cs
+++ src/Services/Ordering/Ordering.Infrastructure/DependancyInjection.cs
@@ -5,13 +5,13 @@ namespace Ordering.Infrastructure;
 
 public static class DependancyInjection
 {
-    public static IServiceCollection AddInfrastructureServices(this IServiceCollection services, IConfiguration configuration)
-        {
+    public static IServiceCollection AddInfrastructureServices
+        (this IServiceCollection services, IConfiguration configuration)
+    {
         var connectionString = configuration.GetConnectionString("Database");
 
-        //services.AddDbContext<ApplicationDbContext>(options =>
-        //    options.UseSqlServer(connectionString,
-        //    b => b.MigrationsAssembly(typeof(OrderingContext).Assembly.FullName)));
+        services.AddDbContext<ApplicationDbContext>(options =>
+            options.UseSqlServer(connectionString));
 
         //services.AddScoped<IApplicationDbContext, ApplicationDbContext>();
 
