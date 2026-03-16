eccd2eeae2cdf96f1e16adc069c4a4541c49cb42
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-03-16 10:19:05 +0200

    create some ordering dtos and create order command

---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderCommand.cs
@@ -0,0 +1,22 @@
+﻿using BuildingBlocks.CQRS;
+using FluentValidation;
+using Ordering.Application.Dtos;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+
+public record CreateORderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+
+public record CreateOrderResult(Guid Id);
+
+public class CreateOrderCommandValidator : AbstractValidator<CreateORderCommand>
+{
+    public CreateOrderCommandValidator()
+    {
+        RuleFor(x => x.Order).NotNull().WithMessage("Order cannot be null");
+        RuleFor(x => x.Order.OrderName).NotNull().WithMessage("Order name cannot be null");
+        RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId cannot be null");
+        RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems cannot be null");
+    }
+}
+
---
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/CreateOrder/CreateOrderHandler.cs
@@ -0,0 +1,14 @@
+﻿using BuildingBlocks.CQRS;
+
+namespace Ordering.Application.Orders.Commands.CreateOrder;
+
+public class CreateOrderHandler : ICommandHandler<CreateORderCommand, CreateOrderResult>
+{
+    public Task<CreateOrderResult> Handle(CreateORderCommand request, CancellationToken cancellationToken)
+    {
+        //create order entity from command object
+        //save to database
+        //return result with order id
+        throw new NotImplementedException();
+    }
+}
