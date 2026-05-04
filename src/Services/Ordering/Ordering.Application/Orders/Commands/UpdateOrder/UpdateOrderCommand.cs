c78e76ea48f1081bf5533ff7b91422e33dc5226c
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-05-04 16:30:22 +0300

    develop update order command and handler

--- src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
+++ src/Services/Ordering/Ordering.Application/Orders/Commands/UpdateOrder/UpdateOrderCommand.cs
@@ -1,20 +1,19 @@
 ﻿using FluentValidation;
 
-namespace Ordering.Application.Orders.Commands.CreateOrder;
+namespace Ordering.Application.Orders.Commands.UpdateOrder;
 
+public record UpdateOrderCommand(OrderDto Order) : ICommand<UpdateOrderResult>;
 
-public record CreateOrderCommand(OrderDto Order) : ICommand<CreateOrderResult>;
+public record UpdateOrderResult(bool IsSuccess);
 
-public record CreateOrderResult(Guid Id);
-
-public class CreateOrderCommandValidator : AbstractValidator<CreateOrderCommand>
+public class UpdateOrderCommandValidator : AbstractValidator<UpdateOrderCommand>
 {
-    public CreateOrderCommandValidator()
+    public UpdateOrderCommandValidator()
     {
         RuleFor(x => x.Order).NotNull().WithMessage("Order cannot be null");
+        RuleFor(x => x.Order.Id).NotNull().WithMessage("Order Id cannot be null");
         RuleFor(x => x.Order.OrderName).NotNull().WithMessage("Order name cannot be null");
         RuleFor(x => x.Order.CustomerId).NotNull().WithMessage("CustomerId cannot be null");
         RuleFor(x => x.Order.OrderItems).NotEmpty().WithMessage("OrderItems cannot be null");
     }
 }
-
