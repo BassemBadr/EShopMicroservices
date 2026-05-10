79a2040cde8f1678f70863c72cf0eb135a7d81a6
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-05-10 17:38:12 +0300

    develop get orders endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrders.cs
@@ -1,10 +1,33 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using BuildingBlocks.Pagination;
+using Ordering.Application.Orders.Queries.GetOrders;
+
+namespace Ordering.API.Endpoints;
 
 //- Accepts pagination parameters
 //- Constructs a GetOrderQuery with the provided parameters
 //- Uses MediatR to  send the query to the corresponding handler
 //- Returns a response with the list of paginated orders
 
-public class GetOrders
+//public record GetOrdersRequest(PaginationRequest PaginationRequest);
+
+public record GetOrdersResponse(PaginatedResult<OrderDto> Orders);
+
+public class GetOrders : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders", async ([AsParameters] PaginationRequest request, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersQuery(request));
+
+            var response = result.Adapt<GetOrdersResponse>();
+
+            return Results.Ok(response);
+        })
+        .WithName("GetOrders")
+        .Produces<GetOrdersResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get orders")
+        .WithDescription("Retrieves a paginated list of orders.");
+    }
 }
