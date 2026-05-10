574711a8532175b4941f5abbe3d08461b329c8b9
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-05-10 17:31:32 +0300

    develop get orders by customer endpoint

--- src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
+++ src/Services/Ordering/Ordering.API/Endpoints/GetOrdersByCustomer.cs
@@ -1,10 +1,32 @@
-﻿namespace Ordering.API.Endpoints;
+﻿using Ordering.Application.Orders.Queries.GetOrdersByCustomer;
+
+namespace Ordering.API.Endpoints;
 
 //- Accepts a customer Id
 //- Constructs a GetOrdersByCustomerQuery with the provided parameters
 //- Uses MediatR to  send the query to the corresponding handler
 //- Returns matching orders based on the provided customer Id
 
-public class GetOrdersByCustomer
+//public record GetOrdersByCustomerRequest(Guid CustomerId);
+
+public record GetOrdersByCustomerResponse(IEnumerable<OrderDto> Orders);
+
+public class GetOrdersByCustomer : ICarterModule
 {
+    public void AddRoutes(IEndpointRouteBuilder app)
+    {
+        app.MapGet("/orders/customer/{customerId}", async (Guid customerId, ISender sender) =>
+        {
+            var result = await sender.Send(new GetOrdersByCustomerQuery(customerId));
+            
+            var response = result.Adapt<GetOrdersByCustomerResponse>();
+            
+            return Results.Ok(response);
+        })
+        .WithName("GetOrdersByCustomer")
+        .Produces<GetOrdersByCustomerResponse>(StatusCodes.Status200OK)
+        .ProducesProblem(StatusCodes.Status400BadRequest)
+        .WithSummary("Get orders by customer")
+        .WithDescription("Retrieves orders based on the provided customer Id.");
+    }
 }
