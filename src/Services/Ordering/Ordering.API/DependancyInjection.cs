52d491ba6bbba5a794e850da02bebae68613dcaf
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-05-17 18:45:40 +0300

    add ordering api and sql server health checks

--- src/Services/Ordering/Ordering.API/DependancyInjection.cs
+++ src/Services/Ordering/Ordering.API/DependancyInjection.cs
@@ -1,15 +1,19 @@
 ﻿using BuildingBlocks.Exceptions.Handler;
 using Carter;
+using HealthChecks.UI.Client;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 
 namespace Ordering.API;
 
 public static class DependancyInjection
 {
-    public static IServiceCollection AddApiServices(this IServiceCollection services)
+    public static IServiceCollection AddApiServices(this IServiceCollection services, IConfiguration configuration)
     {
         var assembly = typeof(Program).Assembly;
         services.AddCarter(new DependencyContextAssemblyCatalog([assembly]));
         services.AddExceptionHandler<CustomExceptionHandler>();
+        services.AddHealthChecks()
+            .AddSqlServer(configuration.GetConnectionString("Database")!);
         return services;
     }
 
@@ -17,6 +21,11 @@ public static class DependancyInjection
     {
         app.MapCarter();
         app.UseExceptionHandler(options => { });
+        app.MapHealthChecks("/health",
+            new HealthCheckOptions
+            {
+                ResponseWriter = UIResponseWriter.WriteHealthCheckUIResponse
+            });
         return app;
     }
 }
