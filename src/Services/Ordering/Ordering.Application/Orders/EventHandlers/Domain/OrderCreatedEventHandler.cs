08738749be7e537ac0492aa82a4bf28f711c8105
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-05-20 01:38:45 +0300

    setup feature management on order fullfilment flow after order is created to enable / disable raising order created integration event

--- src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
+++ src/Services/Ordering/Ordering.Application/Orders/EventHandlers/Domain/OrderCreatedEventHandler.cs
@@ -1,18 +1,22 @@
 ﻿using MassTransit;
+using Microsoft.FeatureManagement;
 
 namespace Ordering.Application.Orders.EventHandlers.Domain;
 
 public class OrderCreatedEventHandler
-    (IPublishEndpoint publishEndpoint, ILogger<OrderCreatedEventHandler> logger)
+    (IPublishEndpoint publishEndpoint, IFeatureManager featureManager, ILogger<OrderCreatedEventHandler> logger)
     : INotificationHandler<OrderCreatedEvent>
 {
     public async Task Handle(OrderCreatedEvent domainEvent, CancellationToken cancellationToken)
     {
         logger.LogInformation("Domain event handled: {DomainEvent}", domainEvent.GetType().Name);
 
-        // TODO: instead of mapping to OrderDto, we can create a specific OrderCreatedIntegrationEvent that contains only the necessary information for the integration event.
-        var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
+        if (await featureManager.IsEnabledAsync("OrderFullfilment"))
+        {
+            // TODO: instead of mapping to OrderDto, we can create a specific OrderCreatedIntegrationEvent that contains only the necessary information for the integration event.
+            var orderCreatedIntegrationEvent = domainEvent.order.ToOrderDto();
 
-        await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+            await publishEndpoint.Publish(orderCreatedIntegrationEvent, cancellationToken);
+        }
     }
 }
