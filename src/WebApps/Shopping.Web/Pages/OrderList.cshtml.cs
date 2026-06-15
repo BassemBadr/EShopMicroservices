49d0cc7173d09f5f0f6c5e27ea0438a8b2932bd4
Basem Mohamed Mahmoud <basem.badr87@gmail.com>
2026-06-15 18:10:31 +0300

    develop order list page and contact page in client web app

---
+++ src/WebApps/Shopping.Web/Pages/OrderList.cshtml.cs
@@ -0,0 +1,22 @@
+using Shopping.Web.Models.Ordering;
+
+namespace Shopping.Web.Pages
+{
+    public class OrderListModel
+        (IOrderingService orderingService, ILogger<OrderListModel> logger)
+        : PageModel
+    {
+        public IEnumerable<OrderModel> Orders { get; set; } = [];
+
+        public async Task<IActionResult> OnGetAsync()
+        {
+            //TODO: repalce customerId for one from authentication when implemented
+            var customerId = new Guid("58c49479-ec65-4de2-86e7-033c546291aa");
+
+            var response = await orderingService.GetOrdersByCustomer(customerId);
+            Orders = response.Orders;
+
+            return Page();
+        }
+    }
+}
